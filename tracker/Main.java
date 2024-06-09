6230aca858e491e8f95a717f36fd98e4941909eb
Zachariah Russell <26415974+Russellzj@users.noreply.github.com>
2024-06-09 15:36:51 -0400

    Created simple user interface to ask for input

--- tracker/Main.java
+++ tracker/Main.java
@@ -6,19 +6,17 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Learning Progre Tracker");
+        System.out.println("Learning Progress Tracker");
         boolean running = true;
         while (running) {
-            String userCommand = sc.next();
-            switch (userCommand) {
-                case "exit":
-                    running = false;
-                    System.out.println("Bye!");
-                    break;
-                case "":
-                    System.out.println("No input");
-                default:
-                    System.out.println("Unknown command!");
+            String userCommand = sc.nextLine();
+            if (userCommand.equals("exit")) {
+                System.out.println("Bye!");
+                break;
+            } else if (userCommand.isBlank()) {
+                System.out.println("No input.");
+            } else {
+                System.out.println("Error: Unknown command!");
             }
         }
 
