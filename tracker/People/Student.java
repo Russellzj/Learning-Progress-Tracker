8450db9abc9d0de76d4de7ecfecc294d40e11c20
Zachariah Russell <26415974+Russellzj@users.noreply.github.com>
2024-06-13 10:37:50 -0400

    created Student class with constructor

--- tracker/People/Student.java
+++ tracker/People/Student.java
@@ -1,2 +1,21 @@
-package tracker.People;public class Student {
+package tracker.People;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public Student(String unorganizedStudentInfo) {
+        this.firstName = unorganizedStudentInfo.substring(0,
+                unorganizedStudentInfo.indexOf(" "));
+        this.lastName = unorganizedStudentInfo.substring(
+                unorganizedStudentInfo.indexOf(" ") + 1,
+                unorganizedStudentInfo.lastIndexOf(" "));
+        this.email = unorganizedStudentInfo.substring(
+                unorganizedStudentInfo.lastIndexOf(" ") + 1);
+    }
+
+    public String getFirstName() {return firstName;}
+    public String getLastName() {return lastName;}
+    public String getEmail() {return email;}
 }
