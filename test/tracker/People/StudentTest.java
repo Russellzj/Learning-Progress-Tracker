00696aed984c23775eb1673180e2305086c01fba
Zachariah Russell <26415974+Russellzj@users.noreply.github.com>
2024-06-13 23:54:00 -0400

    Change test to also test for Middle initial

--- test/tracker/People/StudentTest.java
+++ test/tracker/People/StudentTest.java
@@ -6,9 +6,9 @@ import static org.junit.jupiter.api.Assertions.*;
 class StudentTest {
     @Test
     public void testStudentConstructor() {
-        Student student = new Student("Grace Hopper hopperg@nku.edu");
+        Student student = new Student("Grace J Hopper hopperg@nku.edu");
         assertEquals("Grace", student.getFirstName());
-        assertEquals("Hopper", student.getLastName());
+        assertEquals("J Hopper", student.getLastName());
         assertEquals("hopperg@nku.edu", student.getEmail());
     }
 }
