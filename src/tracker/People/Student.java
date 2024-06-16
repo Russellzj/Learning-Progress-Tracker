6da95eb3e735273bb54e0fb7d5e79efa95261fc7
Zachariah Russell <26415974+Russellzj@users.noreply.github.com>
2024-06-16 14:13:33 -0400

    remove speration of input from Student Class to Main Class

--- src/tracker/People/Student.java
+++ src/tracker/People/Student.java
@@ -5,14 +5,10 @@ public class Student {
     private String lastName;
     private String email;
 
-    public Student(String unorganizedStudentInfo) {
-        this.firstName = unorganizedStudentInfo.substring(0,
-                unorganizedStudentInfo.indexOf(" "));
-        this.lastName = unorganizedStudentInfo.substring(
-                unorganizedStudentInfo.indexOf(" ") + 1,
-                unorganizedStudentInfo.lastIndexOf(" "));
-        this.email = unorganizedStudentInfo.substring(
-                unorganizedStudentInfo.lastIndexOf(" ") + 1);
+    public Student(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
     }
 
     public String getFirstName() {return firstName;}
