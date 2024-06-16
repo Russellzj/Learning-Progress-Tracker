7c7ef2a103c7a122ea044774be7fb1362ee0ebbe
Zachariah Russell <26415974+Russellzj@users.noreply.github.com>
2024-06-16 14:22:16 -0400

    Removed test for student constructor

--- test/tracker/People/StudentTest.java
+++ test/tracker/People/StudentTest.java
@@ -4,11 +4,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StudentTest {
-    @Test
-    public void testStudentConstructor() {
-        Student student = new Student("Grace J Hopper hopperg@nku.edu");
-        assertEquals("Grace", student.getFirstName());
-        assertEquals("J Hopper", student.getLastName());
-        assertEquals("hopperg@nku.edu", student.getEmail());
-    }
+    //tests the constructor of the Student class
+
 }
