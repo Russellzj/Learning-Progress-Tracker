7f51b738c56c6d4a9ec99ce40cd1e91b3050ed9f
Zachariah Russell <russellz1005@gmail.com>
2024-08-09 14:32:44 -0400

    Added method to get most popular class

--- src/tracker/SchoolClass.java
+++ src/tracker/SchoolClass.java
@@ -10,13 +10,52 @@ public class SchoolClass {
     private ArrayList<Integer> spring = new ArrayList<>();
 
     public void addPoint(int[] points) {
-        java.add(points[0]);
-        dsa.add(points[1]);
-        databases.add(points[2]);
-        spring.add(points[3]);
+        if (points[0] != 0 ) {
+            java.add(points[0]);
+        }
+        if (points[1] != 0 ) {
+            dsa.add(points[1]);
+        }
+        if (points[2] != 0 ) {
+            databases.add(points[2]);
+        }
+        if (points[3] != 0 ) {
+            spring.add(points[3]);
+        }
     }
 
-    
+    public String mostPopular(){
+        if (java.size() == 0 && dsa.size() == 0 && databases.size() == 0 && spring.size() == 0) {
+            return "n/a";
+        }
+        String mostPopular;
+        if (java.size() > dsa.size()) {
+            if (java.size() > databases.size()) {
+                if (java.size() > spring.size()) {
+                    mostPopular = "Java";
+                } else {
+                    mostPopular = "Spring";
+                }
+            } else if (databases.size() > spring.size()) {
+                mostPopular = "Databases";
+            } else {
+                mostPopular = "Spring";
+            }
+        } else if (dsa.size() > databases.size()) {
+            if (dsa.size() > spring.size()) {
+                mostPopular = "DSA";
+            } else {
+                mostPopular = "Spring";
+            }
+        } else if (databases.size() > spring.size()) {
+            mostPopular = "Databases";
+        } else {
+            mostPopular = "Spring";
+        }
+        return "Most Popular: " + mostPopular;
+    }
+
+
 
 
 
