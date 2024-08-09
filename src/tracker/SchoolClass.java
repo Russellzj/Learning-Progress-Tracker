42f971d699499f86ffc298e38885fdcfcec71989
Zachariah Russell <26415974+Russellzj@users.noreply.github.com>
2024-08-09 14:12:40 -0400

    Create new class to hold schoolClasses and their functions

--- src/tracker/SchoolClass.java
+++ src/tracker/SchoolClass.java
@@ -1,2 +1,23 @@
-package tracker;public class SchoolClass {
+package tracker;
+
+import java.util.ArrayList;
+
+public class SchoolClass {
+    //The following Collections hold the scores that students get in each class
+    private ArrayList<Integer> java = new ArrayList<>();
+    private ArrayList<Integer> dsa = new ArrayList<>();
+    private ArrayList<Integer> databases = new ArrayList<>();
+    private ArrayList<Integer> spring = new ArrayList<>();
+
+    public void addPoint(int[] points) {
+        java.add(points[0]);
+        dsa.add(points[1]);
+        databases.add(points[2]);
+        spring.add(points[3]);
+    }
+
+    
+
+
+
 }
